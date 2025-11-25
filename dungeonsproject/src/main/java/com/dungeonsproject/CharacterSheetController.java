2cc13611ad97ebef3248cb9ae86e5514c37a563e
Jared Frank <jafrank2@illinois.edu>
2025-11-25 16:20:54 -0600

    add more details to character sheet

--- dungeonsproject/src/main/java/com/dungeonsproject/CharacterSheetController.java
+++ dungeonsproject/src/main/java/com/dungeonsproject/CharacterSheetController.java
@@ -1,15 +1,55 @@
 package com.dungeonsproject;
 
 import java.io.IOException;
+
+import com.dungeonsproject.characterdata.CharacterSheet;
+import com.dungeonsproject.characterdata.CharacterSheet.Stats;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CharacterSheetController {
 
+    //Labels for Character Info
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label levelLabel;
+    @FXML
+    private Label classLabel;
+    @FXML
+    private Label raceLabel;
+
+    //Labels for Stats
+    @FXML
+    private Label strValue;
+    @FXML
+    private Label strMod;
+    @FXML
+    private Label dexValue;
+    @FXML
+    private Label dexMod;
+    @FXML
+    private Label conValue;
+    @FXML
+    private Label conMod;
+    @FXML
+    private Label intValue;
+    @FXML
+    private Label intMod;
+    @FXML
+    private Label wisValue;
+    @FXML
+    private Label wisMod;
+    @FXML
+    private Label chaValue;
+    @FXML
+    private Label chaMod;
+
+    //HP Tracker
     @FXML
     private Label currentHpLabel;
-
     @FXML
     private TextField hpInput;
 
@@ -19,7 +59,29 @@ public class CharacterSheetController {
 
     @FXML
     public void initialize() {
-        currentHpLabel.setText(healthString + App.getCharacterSheet().getCurrentHp());
+        CharacterSheet sheet = App.getCharacterSheet();
+
+        nameLabel.setText(sheet.getName());
+        levelLabel.setText("" + sheet.getLevel());
+        classLabel.setText(sheet.getSubClass() + " " + sheet.getCharClass());
+        raceLabel.setText(sheet.getSubRace() + " " + sheet.getRace());
+        currentHpLabel.setText(healthString + sheet.getCurrentHp());
+
+        Stats stats = sheet.getStats();
+
+        strMod.setText("(" + stats.getStr() + ")");
+        dexMod.setText("(" + stats.getDex() + ")");
+        conMod.setText("(" + stats.getCon() + ")");
+        intMod.setText("(" + stats.getItl() + ")");
+        wisMod.setText("(" + stats.getWis() + ")");
+        chaMod.setText("(" + stats.getCha() + ")");
+
+        strValue.setText(calculateModifier(stats.getStr()));
+        dexValue.setText(calculateModifier(stats.getDex()));
+        conValue.setText(calculateModifier(stats.getCon()));
+        intValue.setText(calculateModifier(stats.getItl()));
+        wisValue.setText(calculateModifier(stats.getWis()));
+        chaValue.setText(calculateModifier(stats.getCha()));
     }
 
     @FXML
@@ -46,6 +108,15 @@ public class CharacterSheetController {
             currentHealth = maxHealth;
         }
         currentHpLabel.setText(healthString + currentHealth);
+    }
+
+    private String calculateModifier(int value) {
+        int calculatedMod = (value - 10) / 2;
+        if (calculatedMod > 0) {
+            return "+" + calculatedMod;
+        } else {
+            return "" + calculatedMod;
+        }
     }
     
 }
--- dungeonsproject/src/main/java/com/dungeonsproject/characterdata/CharacterSheet.java
+++ dungeonsproject/src/main/java/com/dungeonsproject/characterdata/CharacterSheet.java
@@ -1,62 +1,194 @@
 package com.dungeonsproject.characterdata;
 
+import java.util.List;
+
 public class CharacterSheet {
+    private String name;
+    private int level;
+    private String charClass;
+    private String race;
+    private String subRace;
     private int currentHp;
     private int maxHp;
+    private int speed;
+    private Stats stats;
+    private List<String> skills;
+    private String background;
+    private String subClass;
+    private HitDice hitDice;
+
+    public static class Stats {
+        private int str;
+        private int dex;
+        private int con;
+        private int itl;
+        private int wis;
+        private int cha;
+        
+        public int getStr() {
+            return str;
+        }
+        public void setStr(int str) {
+            this.str = str;
+        }
+        public int getDex() {
+            return dex;
+        }
+        public void setDex(int dex) {
+            this.dex = dex;
+        }
+        public int getCon() {
+            return con;
+        }
+        public void setCon(int con) {
+            this.con = con;
+        }
+        public int getItl() {
+            return itl;
+        }
+        public void setItl(int itl) {
+            this.itl = itl;
+        }
+        public int getWis() {
+            return wis;
+        }
+        public void setWis(int wis) {
+            this.wis = wis;
+        }
+        public int getCha() {
+            return cha;
+        }
+        public void setCha(int cha) {
+            this.cha = cha;
+        }
+    }
     
-    private int strStat;
-    private int dexStat;
-    private int conStat;
-    private int intStat;
-    private int wisStat;
-    private int chaStat;
+    public static class HitDice {
+        private int quantity;
+        private int type;
+        private int bonus;
+
+        public int getQuantity() {
+            return quantity;
+        }
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+        public int getType() {
+            return type;
+        }
+        public void setType(int type) {
+            this.type = type;
+        }
+        public int getBonus() {
+            return bonus;
+        }
+        public void setBonus(int bonus) {
+            this.bonus = bonus;
+        }
+    }
 
     public int getCurrentHp() {
         return currentHp;
     }
+
     public void setCurrentHp(int currentHp) {
         this.currentHp = currentHp;
     }
+
     public int getMaxHp() {
         return maxHp;
     }
+
     public void setMaxHp(int maxHp) {
         this.maxHp = maxHp;
     }
-    public int getStrStat() {
-        return strStat;
+
+    public Stats getStats() {
+        return stats;
     }
-    public void setStrStat(int strStat) {
-        this.strStat = strStat;
+
+    public void setStats(Stats stats) {
+        this.stats = stats;
     }
-    public int getDexStat() {
-        return dexStat;
+
+    public List<String> getSkills() {
+        return skills;
     }
-    public void setDexStat(int dexStat) {
-        this.dexStat = dexStat;
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
     }
-    public int getConStat() {
-        return conStat;
+
+    public String getCharClass() {
+        return charClass;
     }
-    public void setConStat(int conStat) {
-        this.conStat = conStat;
+
+    public void setCharClass(String charClass) {
+        this.charClass = charClass;
     }
-    public int getIntStat() {
-        return intStat;
+
+    public int getLevel() {
+        return level;
     }
-    public void setIntStat(int intStat) {
-        this.intStat = intStat;
+
+    public void setLevel(int level) {
+        this.level = level;
     }
-    public int getWisStat() {
-        return wisStat;
+
+    public String getName() {
+        return name;
     }
-    public void setWisStat(int wisStat) {
-        this.wisStat = wisStat;
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public int getChaStat() {
-        return chaStat;
+
+    public String getRace() {
+        return race;
     }
-    public void setChaStat(int chaStat) {
-        this.chaStat = chaStat;
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
+    }
+
+    public String getSubClass() {
+        return subClass;
+    }
+
+    public void setSubClass(String subClass) {
+        this.subClass = subClass;
+    }
+
+    public HitDice getHitDice() {
+        return hitDice;
+    }
+
+    public void setHitDice(HitDice hitDice) {
+        this.hitDice = hitDice;
+    }
+
+    public String getSubRace() {
+        return subRace;
+    }
+
+    public void setSubRace(String subRace) {
+        this.subRace = subRace;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 }
